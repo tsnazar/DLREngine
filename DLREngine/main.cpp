df47a94ff4648168fa58a321d20a74b7c9fd8343
tsnazar <tsnazr3@gmail.com>
2022-04-27 13:24:29 +0300

    Create Window

--- DLREngine/main.cpp
+++ DLREngine/main.cpp
@@ -1,8 +1,53 @@
 #include <Windows.h>
+#include <Windowsx.h>
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
 {
-	MessageBox(NULL, L"Hello World!", L"Just another Hello World Program!", MB_ICONEXCLAMATION | MB_OK);
-	
-	return 0;
+	HWND hWnd;
+	WNDCLASSEX wc;
+
+	ZeroMemory(&wc, sizeof(WNDCLASSEX));
+
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc = WindowProc;
+	wc.hInstance = hInstance;
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+	wc.lpszClassName = L"WindowClass1";
+
+	RegisterClassEx(&wc);
+
+	hWnd = CreateWindowEx(NULL,
+		L"WindowClass1",
+		L"FirstWindow",
+		WS_OVERLAPPEDWINDOW,
+		300, 300, 500, 400, NULL, NULL, hInstance, NULL);
+
+	ShowWindow(hWnd, nShowCmd);
+
+	MSG msg;
+
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return msg.wParam;
+}
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_DESTROY:
+	{
+		PostQuitMessage(0);
+		return 0;
+	} break;
+	}
+	return DefWindowProc(hWnd, message, wParam, lParam);
 }
